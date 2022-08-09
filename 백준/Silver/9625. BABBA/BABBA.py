03ffe063bda5bf10049661b8f85b6854b9d3e30d
movinggyu <107202466+movinggyu@users.noreply.github.com>
2022-08-09 22:56:09 +0900

    [Silver V] Title: BABBA, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,11 @@
+import sys
+
+fibo1 = 0
+fibo2 = 1
+num = int(sys.stdin.readline())
+if num>1:
+    for i in range(num-1):
+        temp = fibo2
+        fibo2 = fibo1+fibo2
+        fibo1 = temp
+print(fibo1, fibo2)
